bd1abd6c83ac50fd550fa71b7649c1421342f81d
guilhermefill <63548453+guilhermefill@users.noreply.github.com>
2022-06-10 16:10:52 +0200

    email work

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,13 +1,25 @@
-import { Button, Container, Heading, Input, Textarea } from '@chakra-ui/react';
+import {
+	Button,
+	Container,
+	Heading,
+	Input,
+	Text,
+	Textarea,
+} from '@chakra-ui/react';
 import React from 'react';
 import emailjs from '@emailjs/browser';
 
 const Contact = ({ id }) => {
 	const form = React.useRef();
+	const [name, setName] = React.useState('');
+	const [subject, setSubject] = React.useState('');
+	const [email, setEmail] = React.useState('');
+	const [message, setMessage] = React.useState('');
+	const [success, setSuccess] = React.useState(false);
 
 	const sendEmail = (e) => {
 		e.preventDefault();
-		console.log(form.current);
+
 		emailjs
 			.sendForm(
 				process.env.SERVICE_ID,
@@ -17,7 +29,13 @@ const Contact = ({ id }) => {
 			)
 			.then(
 				(result) => {
-					console.log(result.text);
+					if (result.text === 'OK') {
+						setName('');
+						setSubject('');
+						setEmail('');
+						setMessage('');
+						setSuccess(true);
+					}
 				},
 				(error) => {
 					console.log(error.text);
@@ -37,6 +55,8 @@ const Contact = ({ id }) => {
 						mb={5}
 						isRequired
 						name="from_name"
+						value={name}
+						onChange={(e) => setName(e.target.value)}
 					></Input>
 					<Input
 						placeholder="Subject"
@@ -44,6 +64,8 @@ const Contact = ({ id }) => {
 						mb={5}
 						isRequired
 						name="subject"
+						value={subject}
+						onChange={(e) => setSubject(e.target.value)}
 					></Input>
 					<Input
 						placeholder="Email"
@@ -51,6 +73,8 @@ const Contact = ({ id }) => {
 						mb={5}
 						isRequired
 						name="reply_to_email"
+						value={email}
+						onChange={(e) => setEmail(e.target.value)}
 					></Input>
 					<Textarea
 						placeholder="Message"
@@ -58,7 +82,16 @@ const Contact = ({ id }) => {
 						mb={5}
 						isRequired
 						name="message"
+						value={message}
+						onChange={(e) => setMessage(e.target.value)}
 					></Textarea>
+					{success ? (
+						<Text fontFamily="Montserrat" p={5}>
+							Email sent successfully
+						</Text>
+					) : (
+						<></>
+					)}
 					<Button type="submit" variant="outline" mb={5} colorScheme="whatsapp">
 						Send Email
 					</Button>
